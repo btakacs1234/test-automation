9f017aad7581a7c61602a24ade01eda28ee5662e
ben <dtn125@alumni.ku.dk>
2023-11-25 12:56:30 +0000

    adding stuff

--- test_sum_unittest.py
+++ test_sum_unittest.py
@@ -1,4 +1,5 @@
 import unittest
+import pytest
 
 class TestSum(unittest.TestCase):
 
@@ -8,5 +9,15 @@ class TestSum(unittest.TestCase):
     def test_sum_tuple(self):
         self.assertEqual(sum((1, 2, 2)), 6, 'Should be 6')
 
+def test_sum():
+    assert sum([1, 4, 6]) == 10, 'Should be 11'
+
+
+
 if __name__ == '__main__':
-    unittest.main()
+    # unittest.main()
+    test_sum()
+
+# Calling nose2 and pytest from the terminal:
+# nose2 - python3 -m nose2, will read test*.py files and test cases inherited from unittest.TestCase in the current directory
+# pytest - python3 -m pytest, will run test cases from files starting with test_, uses built-in assert statement, filter test cases, rerun from last failing test
